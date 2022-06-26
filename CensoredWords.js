e62b1af376aed0bedcfd389d736e0a3ff348cea8
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-06-26 12:50:33 +0300

    Add problem solution

---
+++ CensoredWords.js
@@ -0,0 +1,12 @@
+function censoredWords(text, word) {
+    let newWord = '';
+    for (let i = 0; i < word.length; i++) {
+        newWord += '*';
+    }
+
+    while (text.includes(word)) {
+        text = text.replace(word, newWord);
+    }
+    console.log(text);
+}
+censoredWords('A small sentence with some words', 'small');
