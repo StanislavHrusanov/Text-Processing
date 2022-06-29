0a05a3369ec9041422f76d80eacbbcb134eecef0
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-06-29 10:49:07 +0300

    Add problem solution

---
+++ ModernTimesOf#(HashTag).js
@@ -0,0 +1,31 @@
+function hashTag(str) {
+    str = str.split(' ');
+
+    for (let word of str) {
+
+        if (word[0] == '#' && word.length > 1) {
+            word = word.substring(1, word.length);
+            if (isValidWord(word)) {
+                console.log(word);
+            }
+        }
+    }
+    function isValidWord(word) {
+
+        word = word.split('');
+        let isValid = true;
+
+        for (let letter of word) {
+
+            if ((letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) ||
+                (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122)) {
+                continue;
+            } else {
+                isValid = false;
+                break;
+            }
+        }
+        return isValid;
+    }
+}
+hashTag('The symbol # is known #vari2ously in English-speaking #regions as the #number sign');
